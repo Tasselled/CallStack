11becf81c12a7c8a404b1098b3b2144d922324f6
Evram Dawd <evramdawd@evrams-mbp.myfiosgateway.com>
2023-05-15 12:48:05 -0400

    working on main page router middleware functions

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,5 +1,34 @@
-const User = require('../models/models');
+const {User} = require('../models/models');
+const {Post} = require('../models/models');
 
 const mainController = {};
 
+mainController.getAllPosts = (req, res, next) => {
+    
+  return next();
+
+  return next({
+    log: `getAllPosts: ${err}`,
+    status: 400,
+    message: { err: 'error occurred in mainController.getAllPosts controller' },
+  });
+}
+
+mainController.createPost = (req, res, next) => {
+    const {userId, postTitle, postBody, postTag, numLikes} = req.body;
+
+    Post.create({ userId, postTitle, postBody, postTag, numLikes })
+    .then((post) => {
+      res.locals.post = post.id;
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: `createPost: ${err}`,
+        status: 400,
+        message: { err: 'error occurred in createPost controller' },
+      });
+    });
+  
+}
 module.exports = mainController;
--- server/models/sessionModel.js
+++ server/models/sessionModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema({
   cookieId: { type: String, required: true, unique: true },
-  createdAt: { type: Date,  expires: 3, default: Date.now }
+  createdAt: { type: Date,  expires: 3*60, default: Date.now }
   // expires after 10min (10*60). Can change this to test if the expiration works later.
 });
 
