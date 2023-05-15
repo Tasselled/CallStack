4f508948f59370e9bd53f5d4da4ac62fcb6630fd
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-05-15 10:43:04 -0400

    Fixed merge conflict

---
+++ src/store/userReducer.js
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userReducer = createSlice({
+  name: 'userReducer',
+  initialState: {
+    currentUser: null,
+    currentPost: null,
+    errorMessage: [],
+    tags: [],
+    searchResultPosts: [],
+    allPosts: []
+  },
+
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    setCurrentPost: (state, action) => {
+      state.currentPost = action.payload;
+    },
+    setErrorMessage: (state, action) => {
+      state.errorMessage = action.payload;
+    },
+    setTags: (state, action) => {
+      if (action.payload === 'delete') state.tags = [];
+      else if (state.tags.includes(action.payload))
+        state.tags = state.tags
+          .slice(0, state.tags.indexOf(action.payload))
+          .concat(state.tags.indexOf(action.payload) + 1);
+      else state.tags = [...state.tags, action.payload];
+    },
+    setSearchResultPosts: (state, action) => {
+      state.searchResultPosts = action.payload === 'delete' ? [] : [...state.searchResultPosts, action.payload];
+    },
+    setAllPosts: (state, action) => {
+      state.allPosts = [...state.allPosts, ...action.payload]
+    }
+  },
+});
+
+export const { setCurrentUser, setCurrentPost, setErrorMessage, setTags, setSearchResultPosts, setAllPosts } = userReducer.actions;
+
+export default userReducer.reducer;
