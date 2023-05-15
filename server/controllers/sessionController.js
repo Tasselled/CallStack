8af74f6cbd84b36739231c412c1ca7b05bf6503c
Evram Dawd <evramdawd@evrams-mbp.myfiosgateway.com>
2023-05-14 23:25:39 -0400

    authentication work completed

---
+++ server/controllers/sessionController.js
@@ -0,0 +1,45 @@
+const Session = require('../models/sessionModel');
+
+const sessionController = {};
+
+/*
+isLoggedIn - find appropriate session for this request in DB - verify whether or not session is still valid
+*/ 
+sessionController.isLoggedIn = async (req, res, next) => {
+  console.log('req.cookies: ', req.cookies);
+  
+  // Find ssid cookie in DB to see if user is logged in:
+  try {
+    const session = await Session.findOne({cookieId: req.cookies.ssid});
+
+    if(!session) { // no session found
+      return res.send(false) // Redirect to Signup here? Or login or main landing page?    
+    } else {
+      return next();
+    }
+  } catch {
+    return next({
+      log: 'Error occurred in sessionController.isLoggedIn.',
+      status: 500,
+      message: { err: 'An error occurred' },
+    })
+  }
+};
+
+/*
+startSession - create & save a new Session into the DB
+*/
+sessionController.startSession = async (req, res, next) => {
+  try {
+    await Session.create({cookieId: res.locals.user});
+    return next();
+  } catch {
+    return next({
+      log: 'Error occured in sessionController.startSession' ,
+      status: 500,
+      message: {err: 'An error occurred'},
+    });
+  }
+}
+
+module.exports = sessionController;
