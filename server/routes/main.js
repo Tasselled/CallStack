a2b30f8326ae7d2a7713e019527680b1e3e092c0
Kevin Fan <kevinlifan@yahoo.com>
2023-05-15 11:46:53 -0400

    server login stuff

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mainRouter = express.Router();
 
 // Requiring all controllers
+const mainController = require('../controllers/mainController');
+const sessionController = require('../controllers/sessionController')
+
+mainRouter.get('/get', sessionController.isLoggedIn, (req, res) => {return res.status(200).send("IT WORKS!")});
 
 
-// app.get('/get', sessionController.isLoggedIn, (req, res) => {return res.status(200)});
-
+// mainRouter.get('/main', mainController.getPost )
 
 module.exports = mainRouter;
