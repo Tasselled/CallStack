5a301f7789245626f0ebcf12c784757cdbc69087
Kevin Fan <kevinlifan@yahoo.com>
2023-06-22 11:38:30 -0400

    deployed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const PORT = 3000;
 
 // Parsing urlencoded content in request body
@@ -10,18 +10,18 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Handling request for static files
-app.use("/static", express.static(path.resolve(__dirname, "../src")));
+app.use('/static', express.static(path.resolve(__dirname, '../src')));
 
 // Requiring route handlers
-const mainRouter = require("./routes/main");
-const loginRouter = require("./routes/login");
+const mainRouter = require('./routes/main');
+const loginRouter = require('./routes/login');
 
 // Defining route handlers
-app.use("/login", loginRouter);
-app.use("/main", mainRouter);
+app.use('/login', loginRouter);
+app.use('/main', mainRouter);
 
 // 404 error handler
-app.use("*", (req, res) => {
+app.use('*', (req, res) => {
   res.status(404).send("This is not the page you're looking for");
 });
 
